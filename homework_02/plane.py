69105153f7786f931c7d8f3dc44782ac03a1aef8
Artem Balabashin <BalabashinAN@gmail.com>
2023-11-14 01:25:31 +0500

    update files

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -7,8 +7,6 @@ from homework_02 import exceptions
 
 class Plane(Vehicle):
     cargo = 0
-    max_cargo = 200
-
     def __init__(self, weight, fuel, fuel_consumption, max_cargo):
         super().__init__(weight, fuel, fuel_consumption)
         self.max_cargo = max_cargo
