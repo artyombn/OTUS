23f838bdd22e153edce3a4a58e503fc28e396dcb
artyombn <balabashinan@gmail.com>
2024-03-19 02:51:58 +0500

    last

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,13 +4,14 @@
 """
 
 
-def power_numbers():
+def power_numbers(*args):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return [i ** 2 for i in args]
 
 
 # filter types
@@ -19,7 +20,20 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def check(x):
+    count = 0
+    if x < 0:
+        return False
+    for i in range(2, (x // 2) + 1):
+        if x % i == 0:
+            count += 1
+    if count <= 0 and x != 0 and x != 1:
+        return True
+    else:
+        return False
+
+
+def filter_numbers(input_list, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +44,13 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+
+    if filter_type == ODD:
+        return list(filter(lambda x: x % 2 != 0, input_list))
+    elif filter_type == EVEN:
+        return list(filter(lambda x: x % 2 == 0, input_list))
+    elif filter_type == PRIME:
+        return list(filter(check, input_list))
+    else:
+        print('No filter type')
+        return None
