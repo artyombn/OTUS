1ecf6dbf5879e00c9784f85b18efd0ee217ae144
artyombn <balabashinan@gmail.com>
2024-04-05 00:16:36 +0500

    homework_02 complete

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,13 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
+
+class Car(Vehicle):
+
+    def set_engine(self, engine):
+        if isinstance(engine, Engine):
+            self.engine = engine
+        else:
+            raise f'Engine object only!'
--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -4,3 +4,19 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+class BaseException(Exception):
+    def __init__(self, message="Base Exception"):
+        super().__init__(message)
+
+class LowFuelError(BaseException):
+    def __init__(self):
+        print("Low Fuel")
+
+class NotEnoughFuel(BaseException):
+    def __init__(self):
+        print('Not Enough Fuel')
+
+class CargoOverload(BaseException):
+    def __init__(self):
+        print('Cargo Overloaded')
