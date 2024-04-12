a4d3e39a2de84664b7abf40278fbd90f46c9fc16
artyombn <balabashinan@gmail.com>
2024-04-13 01:16:18 +0500

    add lesson 14 files

---
+++ lessons/lesson_14/app.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from views.hello import router as router_hello
+from views.items import router as router_items
+
+app = FastAPI()
+app.include_router(router_hello)
+app.include_router(router_items)
+
+
+@app.get("/")
+def index_page():
+    return {"message": "Hello World"}
+
+
+@app.get("/demo-html/", response_class=HTMLResponse)
+def demo_html():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <body>
+    
+    <h1>My First Heading</h1>
+    
+    <p>My first paragraph.</p>
+    
+    </body>
+    </html>
+    """
---
+++ lessons/lesson_14/main.py
@@ -0,0 +1,18 @@
+"""
+FastAPI
+https://fastapi.tiangolo.com/ru/
+
+Запустить можно через код или через терминал посредством gunicorn
+https://gunicorn.org
+
+Для асинхронной работы с кодом нужно использовать uvicorn
+https://www.uvicorn.org
+
+"""
+
+__all__ = ("app",)
+from app import app
+
+# uvicorn main:app
+# запускать нужно с каталога, где находится main
+# или: uvicorn lessons.lesson_14.main:app
